24802d9080e052aaf4fd8eef15a2931a9a98f7fa
Luke Steyn <luke@alkirasoftware.com>
2020-04-07 01:23:01 +1000

    Updates

---
+++ precache-manifest.b6c0dd7ea5b07f8ccfc7c16cb6c30473.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "124c947cbd322b9f19d6b89bbc1c25ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7206fbe3537b4f44d345",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8502e209463459aa6415",
+    "url": "/static/js/2.69335e06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.69335e06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7206fbe3537b4f44d345",
+    "url": "/static/js/main.b7c80517.chunk.js"
+  },
+  {
+    "revision": "15fc5f1722c801adb82b",
+    "url": "/static/js/runtime-main.313f0537.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
