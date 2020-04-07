066b0b4e4f43b69816d8ce9256ff0e92cd8a9c6f
Luke Steyn <luke@alkirasoftware.com>
2020-04-07 23:09:14 +1000

    Deleted bad commit from gh-pages package. Test publishing new page (blank react app template) with source code.

---
+++ precache-manifest.cb137ad67111b307d617b0c16c6284fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc0cac9ebc07f4ce2436fe8cac2f6e3",
+    "url": "/lukecaan.github.io/index.html"
+  },
+  {
+    "revision": "059471c76d54b04c1116",
+    "url": "/lukecaan.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8502e209463459aa6415",
+    "url": "/lukecaan.github.io/static/js/2.69335e06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lukecaan.github.io/static/js/2.69335e06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "059471c76d54b04c1116",
+    "url": "/lukecaan.github.io/static/js/main.1f55109e.chunk.js"
+  },
+  {
+    "revision": "642711cd5c8fc97472b0",
+    "url": "/lukecaan.github.io/static/js/runtime-main.48744920.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lukecaan.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b6c0dd7ea5b07f8ccfc7c16cb6c30473.js"
+  "/lukecaan.github.io/precache-manifest.cb137ad67111b307d617b0c16c6284fa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lukecaan.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
