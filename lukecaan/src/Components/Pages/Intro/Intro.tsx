60f11ef10b8e71bd3f57663281d3fd67cd55843c
Luke Steyn <luke@alkirasoftware.com>
2021-02-26 18:06:25 +1100

    Adjusted intro message in portfolio

--- lukecaan/src/Components/Pages/Intro/Intro.tsx
+++ lukecaan/src/Components/Pages/Intro/Intro.tsx
@@ -11,19 +11,12 @@ const Intro = () => {
             <div
                 className={styles.introMessage}
             >
-                {/* <h1>
-                        Introduction
-                    </h1> */}
-
                 <p>
                     Hi, Welcome to my Portfolio.
-                    </p>
+                </p>
                 <p>
-                    If you're reading this and you are from <span className={styles.dapper}>Dapper Labs</span>, then I made this just for you. If you are not from <span className={styles.dapper}>Dapper Labs</span>, then I have clearly settled for applying with other companies. I'll never love you as much as <span className={styles.dapper}>Dapper Labs</span> but I think I'm still a great person to hire.
-                    </p>
-                <p>
-                    I hope this portfolio can give you a brief but effective insight into my personality and skills. It features an abridged version of my CV, and a short overview of two of my personal projects.
-                    </p>
+                    If you're reading this then I'm probably trying to convince you to hire me, so I hope this portfolio can give you a brief but effective insight into my personality and skills. It features an abridged version of my CV, and a short overview of two of my personal projects.
+                </p>
             </div>
 
             <div className={`${styles.cvSection} snapScrollTop`}>
